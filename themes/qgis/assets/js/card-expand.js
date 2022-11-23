1e5e6c97204e29a621d99773657825945fd00ba8
Tim Sutton <tim@kartoza.com>
2022-11-23 00:27:42 +0000

    WIP for extra content toggles

--- themes/qgis/assets/js/card-expand.js
+++ themes/qgis/assets/js/card-expand.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let cardToggles = document.getElementsByClassName('pulldown-content');
     for (let i = 0; i < cardToggles.length; i++) {
         cardToggles[i].addEventListener('click', e => {
+            console.log("Card expander clicked");
             e.currentTarget.parentElement.parentElement.childNodes[3].classList.toggle('is-hidden');
         });
     }
