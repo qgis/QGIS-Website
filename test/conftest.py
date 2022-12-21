57261d17175db61a085a71acdd183793e24284d4
Robert Coffie <rcoffie22@yahoo.com>
2022-12-21 16:05:39 +0000

    testing navbar  [skip ci]

--- test/conftest.py
+++ test/conftest.py
@@ -30,31 +30,30 @@ def expected_rss_feeds() -> List[
 @fixture
 def expected_dropdown_items(base_url) -> List[Dict[str, str]]:
     return [
-        {"href": "/community/", "title": "Community"},
-        {"href": "/guests", "title": "Guests"},
-        {"href": "https://github.com/JupiterBroadcasting/", "title": "GitHub"},
-        {"href": "https://jupiter.tube", "title": "Peertube"},
-        {"href": "https://www.meetup.com/jupiterbroadcasting", "title": "Meetup"},
-        {
-            "href": "https://www.youtube.com/user/jupiterbroadcasting",
-            "title": "YouTube",
-        },
-        {"href": "https://twitter.com/jupitersignal", "title": "Twitter"},
-        {"href": "{}/community/irc".format(base_url), "title": "IRC"},
-        {
-            "href": "http://www.facebook.com/pages/Jupiter-Broadcasting/156241429615",
-            "title": "Facebook",
-        },
-        {"href": "https://discord.com/invite/n49fgkp", "title": "Self-Hosted Discord"},
-        {"href": "{}/community/matrix".format(base_url), "title": "Matrix"},
-        {"href": "{}/community/mumble".format(base_url), "title": "Mumble"},
-        {"href": "https://t.me/jupitertelegram", "title": "Telegram"},
-        {"href": "/show/coder-radio", "title": "Coder Radio"},
-        {"href": "/show/jupiter-extras", "title": "Jupiter EXTRAS"},
-        {"href": "/show/linux-action-news", "title": "Linux Action News"},
-        {"href": "/show/linux-unplugged", "title": "LINUX Unplugged"},
-        {"href": "/show/office-hours", "title": "Office Hours"},
-        {"href": "/show/self-hosted", "title": "Self-Hosted"},
+        {"href": "/download-linux/", "title": "Linux"},
+        {"href": "/download-mac/", "title": "macOS"},
+        {"href": "/download-windows", "title": "Windows"},
+        # {"href": "https://www.meetup.com/jupiterbroadcasting", "title": "Meetup"},
+        # {
+        #     "href": "https://www.youtube.com/user/jupiterbroadcasting",
+        #     "title": "YouTube",
+        # },
+        # {"href": "https://twitter.com/jupitersignal", "title": "Twitter"},
+        # {"href": "{}/community/irc".format(base_url), "title": "IRC"},
+        # {
+        #     "href": "http://www.facebook.com/pages/Jupiter-Broadcasting/156241429615",
+        #     "title": "Facebook",
+        # },
+        # {"href": "https://discord.com/invite/n49fgkp", "title": "Self-Hosted Discord"},
+        # {"href": "{}/community/matrix".format(base_url), "title": "Matrix"},
+        # {"href": "{}/community/mumble".format(base_url), "title": "Mumble"},
+        # {"href": "https://t.me/jupitertelegram", "title": "Telegram"},
+        # {"href": "/show/coder-radio", "title": "Coder Radio"},
+        # {"href": "/show/jupiter-extras", "title": "Jupiter EXTRAS"},
+        # {"href": "/show/linux-action-news", "title": "Linux Action News"},
+        # {"href": "/show/linux-unplugged", "title": "LINUX Unplugged"},
+        # {"href": "/show/office-hours", "title": "Office Hours"},
+        # {"href": "/show/self-hosted", "title": "Self-Hosted"},
     ]
 
 
@@ -70,7 +69,7 @@ def expect_nav_items() -> List[Dict[str, str]]:
     ]
 
     page.goto("/")
-    nav = page.locator("#mainnavigation")
+    nav = page.locator(".navbar")
     expect(nav).to_be_visible()
     dropdown_nav_items = page.locator(".navbar-start > * > a")
     count = dropdown_nav_items.count()
