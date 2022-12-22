821c590ae8960e502b2e76750d28c509c99d9e9b
Robert Coffie <rcoffie22@yahoo.com>
2022-12-22 13:03:54 +0000

    working on downwload button , removing icons from menu , adding animate on scroll feature [skip ci]

--- themes/qgis/assets/js/animate.js
+++ themes/qgis/assets/js/animate.js
@@ -1,24 +1,39 @@
-const qgis_observer = new IntersectionObserver((entries) => {
-    console.log("Animation observer");
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animation-show');
-            entry.target.classList.remove('animation-hide-left');
-            entry.target.classList.remove('animation-hide-right');
-        } else {
-            if (entry.target.classList.contains('animate-from-left'))
-            {
-                entry.target.classList.add('animation-hide-left');
-            };
-            if (entry.target.classList.contains('animate-from-right'))
-            {
-                entry.target.classList.add('animation-hide-right');
-            };            
-            entry.target.classList.remove('animation-show');
+// const qgis_observer = new IntersectionObserver((entries) => {
+//     console.log("Animation observer");
+//     entries.forEach((entry) => {
+//         console.log(entry);
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animation-show');
+//             entry.target.classList.remove('animation-hide-left');
+//             entry.target.classList.remove('animation-hide-right');
+//         } else {
+//             if (entry.target.classList.contains('animate-from-left'))
+//             {
+//                 entry.target.classList.add('animation-hide-left');
+//             };
+//             if (entry.target.classList.contains('animate-from-right'))
+//             {
+//                 entry.target.classList.add('animation-hide-right');
+//             };            
+//             entry.target.classList.remove('animation-show');
             
-        }
-    })
+//         }
+//     })
+// });
+// const animationElements = document.querySelectorAll('.animation-element');
+// animationElements.forEach((element) => qgis_observer.observe(element));
+
+
+// This fades in sections when you are scrolling you add fade class if you want the  item or shortcode to fadein and out when scrolling 
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+    console.log(entry)
+    if (entry.isIntersecting){
+        entry.target.classList.add('show');
+    }else {
+        entry.target.classList.remove('show');
+    }
+    });
 });
-const animationElements = document.querySelectorAll('.animation-element');
-animationElements.forEach((element) => qgis_observer.observe(element));
+const hiddenElements = document.querySelectorAll('.fade');
+hiddenElements.forEach((el)=> observer.observe(el))
