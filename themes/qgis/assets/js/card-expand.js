ed601c53cbcea846f7266f30c25aa25674c78f70
Tim Sutton <tim@kartoza.com>
2023-01-02 11:43:33 +0000

    Image blocks are distored WIP for  #73

--- themes/qgis/assets/js/card-expand.js
+++ themes/qgis/assets/js/card-expand.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i = 0; i < cardToggles.length; i++) {
         cardToggles[i].addEventListener('click', e => {
             console.log("Card expander clicked");
-            e.currentTarget.parentElement.parentElement.childNodes[1].classList.toggle('is-hidden');
+            e.currentTarget.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("content")[0].classList.toggle('is-hidden');
         });
     }
 });
