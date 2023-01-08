e922a221c107c70ee499e8f06610e4a8d9b4805d
Tim Sutton <tim@kartoza.com>
2023-01-08 08:39:42 +0000

    Fix tests

--- test/e2e/test_download-linux.py
+++ test/e2e/test_download-linux.py
@@ -5,21 +5,21 @@ from playwright.sync_api import Page, expect
 
 
 def test_homepage_screenshot(page: Page, screenshot_dir: Path):
-    page.goto("/downloads/linux/")
+    page.goto("/download/linux/")
     page.pause()
     page.screenshot(path=f"{screenshot_dir}/download-linux.png", full_page=True)
 
 
 def test_download_linux_page_title(page: Page):
-    page.goto("/downloads/linux/")
+    page.goto("/download/linux/")
     assert page.title() == "Download · QGIS Web Site"
 
 def test_download_linux_banner_title(page: Page):
-    page.goto("/downloads/linux/")
+    page.goto("/download/linux/")
     hero_title = page.locator(".hero-title")
     expect(hero_title).to_contain_text("Download")
 
 def test_download_linux_banner_subtitle(page: Page):
-    page.goto("/downloads/linux/")
+    page.goto("/download/linux/")
     hero_subtitle = page.locator(".hero-subtitle")
     expect(hero_subtitle).to_contain_text("QGIS Desktop for Linux")
