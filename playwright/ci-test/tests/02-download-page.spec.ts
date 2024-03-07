0d4bf6ea32b9d705e7eee72f5fc30509df591887
sleeping <hh.sleeping@gmail.com>
2024-03-07 19:38:09 +0400

    fix tests once more

--- playwright/ci-test/tests/02-download-page.spec.ts
+++ playwright/ci-test/tests/02-download-page.spec.ts
@@ -6,11 +6,11 @@ test('download page', async ({ page }) => {
   await page.goto(url);
   await page.locator('section').filter({ hasText: 'Free and open source Spatial' }).getByRole('link').click();
   await expect(page.getByText('Before download starts ¶ QGIS')).toBeVisible();
-  await expect(page.getByText('$ 10.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 20.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 50.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 100.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 250.00', { exact: true })).toBeVisible();
+  await expect(page.getByText('$ 10.00').first()).toBeVisible();
+  await expect(page.getByText('$ 20.00').first()).toBeVisible();
+  await expect(page.getByText('$ 50.00').first()).toBeVisible();
+  await expect(page.getByText('$ 100.00').first()).toBeVisible();
+  await expect(page.getByText('$ 250.00').first()).toBeVisible();
   await expect(page.locator('#submit-button')).toBeVisible();
   await expect(page.getByRole('link', { name: 'Other methods, more info' })).toBeVisible();
   await expect(page.locator('#currency')).toBeVisible();
--- playwright/ci-test/tests/05-donate-page.spec.ts
+++ playwright/ci-test/tests/05-donate-page.spec.ts
@@ -11,11 +11,11 @@ test('donate page', async ({ page }) => {
   await expect(page.getByText('We rely on sponsorships and')).toBeVisible();
   await expect(page.getByText('Donations to QGIS are tax-')).toBeVisible();
   await expect(page.getByText('We use the stripe')).toBeVisible();
-  await expect(page.getByText('$ 10.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 20.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 50.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 100.00', { exact: true })).toBeVisible();
-  await expect(page.getByText('$ 250.00', { exact: true })).toBeVisible();
+  await expect(page.getByText('$ 10.00').first()).toBeVisible();
+  await expect(page.getByText('$ 20.00').first()).toBeVisible();
+  await expect(page.getByText('$ 50.00').first()).toBeVisible();
+  await expect(page.getByText('$ 100.00').first()).toBeVisible();
+  await expect(page.getByText('$ 250.00').first()).toBeVisible();
   await expect(page.locator('#submit-button')).toBeVisible();
   await expect(page.locator('#currency')).toBeVisible();
   await expect(page.getByText('Bank transfer ¶ Use the')).toBeVisible();
