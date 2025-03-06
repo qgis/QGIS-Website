a872ce4742c9424cfb0f6233303482883b3087c1
Ivan Mincik <ivan.mincik@gmail.com>
2025-03-06 11:41:39 +0100

    Add Nix package for QGIS website

---
+++ package.nix
@@ -0,0 +1,40 @@
+{ lib
+, stdenv
+, hugo
+}:
+
+stdenv.mkDerivation {
+  name = "qgis-website";
+
+  src = lib.cleanSourceWith {
+    src = ./.;
+    filter = (
+      path: type: (builtins.all (x: x != baseNameOf path) [
+        ".git"
+        ".github"
+        "flake.nix"
+        "flake.lock"
+        "package.nix"
+        "result"
+      ])
+    );
+  };
+
+  buildInputs = [ hugo ];
+
+  buildPhase = ''
+    hugo --config config.toml,config/config.prod.toml
+    hugo --config config.toml,config/config.www.toml
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r public_www public_prod $out/
+  '';
+
+  meta = with lib; {
+    description = "A QGIS website";
+    license = licenses.mit;
+  };
+}
+
